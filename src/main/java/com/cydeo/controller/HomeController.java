e191803a1f82cdf855651d5cadc1911da9d162fb
ELİF DENK <elifdenkk@gmail.com>
2023-08-29 11:10:43 +0300

    home controller fixed

--- src/main/java/com/cydeo/controller/HomeController.java
+++ src/main/java/com/cydeo/controller/HomeController.java
@@ -1,8 +1,15 @@
 package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @GetMapping
+    public String home(){
+
+        return "/home";
+    }
 
 }
